255c9d30824780a43c8ee223b907dd1cce8cf815
Wes Castro <root670@gmail.com>
2012-07-07 23:31:26 -0700

    Add toggle for showing/hiding a code's number, author credit, and notes columns
    To be used by index.php when responding to a request for a code page.

--- config.php
+++ config.php
@@ -1,5 +1,6 @@
 <?php
 /* Dabase Info */
+$host = 'localhost';
 $user = 'root';
 $password = '';
 $database = 'codedb';
@@ -9,3 +10,16 @@ $database = 'codedb';
 // Admin Mode
 // Description: Change this to true to gain the ability of add games and codes to the database.
 $adminMode = true;
+
+// Show Code Numbers
+// Description: Toggle if a number for each code row will appear on a game's code list;
+$showNumbers = true;
+
+// Show Credit
+// Description: Toggle if the credit column will appear on a game's code list.
+$showCredit = false;
+
+// Show Notes
+// Description: Toggle if the note column will appear on a game's code list.
+$showNotes = false;
+?>
